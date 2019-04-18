c0d36ab9786b36740c1ab9542b22137a6f0056e8
José Rodrigues <jyrodrigues@gmail.com>
2019-04-17 21:16:38 -0300

    Import index.html inside index.js to move it to dist via file-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Importing index.html to force webpack copy it to dist folder.
+import './index.html';
+
 const rootNode = document.getElementById('root');
 
 rootNode.innerHTML = 'Here is something from webpack ;)';
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,16 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: "file-loader?name=[name].[ext]",
+      }
+    ]
   }
 };
