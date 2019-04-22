640ae14ded9985e1d3ac005c6adc8968a1cf41da
José Rodrigues <jyrodrigues@gmail.com>
2019-04-22 16:14:13 -0300

    Add empty elm file showing how elm-webpack-loader works

---
+++ src/Index.elm
@@ -0,0 +1,13 @@
+{--
+
+This file only exists to prove that elm-webpack-loader loads
+every .elm file regardless of which one we import inside index.js
+
+--}
+
+
+module EmptyIndex exposing (empty)
+
+
+empty =
+    ""
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // Importing index.html to force webpack copy it to dist folder.
 import './index.html';
 
-import { Elm } from './Main.elm';
+import { Elm } from './Index.elm';
 
-const app = Elm.Main.init({
-// const app = Elm.Nav.init({
+// const app = Elm.Main.init({
+const app = Elm.Nav.init({
   node: document.getElementById('root')
 });
